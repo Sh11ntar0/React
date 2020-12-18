0a9c94e01295df9e12df03adad2e340173c38ded
takayasu.nasu <taka.beckham@gmail.com>
2020-12-18 00:41:51 -0800

    Add styled component.

--- src/ClockWithUse.js
+++ src/ClockWithUse.js
@@ -1,4 +1,15 @@
 import React, { useState, useEffect } from 'react'
+import styled from "styled-components"
+import media from "styled-media-query"
+
+const Text = styled.p`
+font-size: 40px;
+color: lime;
+${media.greaterThan("large")`
+  font-size: 20px;
+  color: deepskyblue;
+`}
+`
 
 const ClockWithUse = props => {
   const [count, setCount] = useState(0)
@@ -12,7 +23,7 @@ const ClockWithUse = props => {
       <h1>Hello, world!</h1>
       <h2>It is {props.date.toLocaleTimeString()}.</h2>
       <button onClick={countUp}>Count UP</button>
-      <p>Count {count}</p>
+      <Text>Count {count}</Text>
     </React.Fragment>
   )
 }
