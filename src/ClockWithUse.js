3416adfdead5d86689175ee322980ddb7c168e8f
takayasu.nasu <taka.beckham@gmail.com>
2021-01-08 18:03:00 -0800

    Modified font color when count more than 10.

--- src/ClockWithUse.js
+++ src/ClockWithUse.js
@@ -7,23 +7,27 @@ font-size: 40px;
 color: lime;
 ${media.greaterThan("large")`
   font-size: 20px;
-  color: deepskyblue;
+  color: ${(props) => props.color};
 `}
 `
 
 const ClockWithUse = props => {
   const [count, setCount] = useState(0)
+  const [fontColor, setFontColor] = useState('deepskyblue')
   const countUp = () => setCount(count + 1)
   useEffect(() => {
     console.log('%cuseEffect', 'color:skyblue; font-size: 20px;')
+    if (count > 9) updateFontColor()
   }, [count])
+
+  const updateFontColor = () => setFontColor('red')
 
   return (
     <React.Fragment>
       <h1>Hello, world!</h1>
       <h2>It is {props.date.toLocaleTimeString()}.</h2>
       <button onClick={countUp}>Count UP</button>
-      <Text>Count {count}</Text>
+      <Text color={fontColor}>Count {count}</Text>
     </React.Fragment>
   )
 }
